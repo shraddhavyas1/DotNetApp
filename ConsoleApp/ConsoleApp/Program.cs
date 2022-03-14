3dd056706c271c449a5d7d46feb434ddf840e8a2
Shraddha <shraddha.vyas@intimetec.com>
2022-03-14 08:53:00 +0530

    DotNet console app push

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace ConsoleApp { 
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World from .net App!!");
+        }
+    }
+}
